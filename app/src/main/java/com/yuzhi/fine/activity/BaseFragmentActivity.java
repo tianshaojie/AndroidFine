ce66d19f29e5275fc6259185b91188a45614ca15
tiansj <tiansj@xuanniu.com>
2016-02-01 18:48:39 +0800

    底部导航添加未读数字

--- app/src/main/java/com/yuzhi/fine/activity/BaseFragmentActivity.java
+++ app/src/main/java/com/yuzhi/fine/activity/BaseFragmentActivity.java
@@ -44,8 +44,8 @@ public class BaseFragmentActivity extends FragmentActivity {
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                 WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         // Translucent navigation bar
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
-                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+//        window.setFlags(
+//                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION,
+//                WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 }
